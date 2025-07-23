2ab63ece1668cf83ea15e8281a3c4391edaa7920
Abdul-Mueed54 <mueed9972@gmail.com>
2025-07-23 11:17:38 +0000

    project on

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 "use strict"; //treat all js code as newer js
 // alert("hello world") // we are using nodejs, not browser
 
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
